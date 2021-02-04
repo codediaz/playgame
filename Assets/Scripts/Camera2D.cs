73400f27e52d22716d1b73e2122f65565936c890
Sergio Díaz <codediaz@outlook.es>
2021-02-04 16:09:15 -0500

    First push to GitHub

---
+++ Assets/Scripts/Camera2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2D : MonoBehaviour
+{
+    public Transform targetPlayer;
+
+    void Start()
+    {
+        if (targetPlayer == null)
+        {
+            targetPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
+
+
+    void Update()
+    {
+        transform.position = new Vector3(targetPlayer.position.x + 6f, 0, -10);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float playerJumpForce = 20f;
+    public float playerSpeed = 5f;
+
+    private Rigidbody2D myRigidbody2D; /*Interaction Components [Object]*/
+    
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>(); /*Take a lot of Components for interaction*/
+    }
+
+
+    void Update() /*Update with one key, in this project with space*/
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody2D.velocity = new Vector2(myRigidbody2D.velocity.x, playerJumpForce);
+        }
+        myRigidbody2D.velocity = new Vector2(playerJumpForce, myRigidbody2D.velocity.y);
+    }
+}
